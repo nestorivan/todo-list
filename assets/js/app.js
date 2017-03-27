55db570e7651fec8f080a8ab5b1e9c0b3b6cf425
Homero Silva <hsilvap@me.com>
2017-03-26 23:53:07 -0600

    Saving new to do
    after pressing the intro key, the new item is saved to the localstorage.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,19 @@
 
     app.controller('addToDo', function () {
         var vm = this;
+        console.log(JSON.parse(localStorage.getItem("list")));
+
         vm.saveToDo = function (key) {
             if (key.keyCode == 13) {
-                console.log(vm.newToDo);
+                var currentList = JSON.parse(localStorage.getItem("list"));
+                if(currentList == null){
+                    toDo = [];
+                }
+                currentList.push({ name: vm.newToDo, complete: false });
+                localStorage.setItem("list", JSON.stringify(currentList))
+                vm.newToDo = '';
             }
+
         };
     });
 })(window.angular);
